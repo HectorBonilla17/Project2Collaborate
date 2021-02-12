4dcbdc89adb292887e316d0ef79b40066c8bcb6f
HectorBonilla17 <31080342+HectorBonilla17@users.noreply.github.com>
2021-02-12 17:41:58 -0600

    Add files via upload

--- Team.py
+++ Team.py
@@ -1,8 +1,11 @@
-def team_create(teamID, teamName, courseID, teamBy):
+def team_create(teamName, choosenCourse):
     try:
         print()
         print("Start team_create():")
-        cursor.execute('INSERT INTO Teams (TeamID , TeamName, CourseID, TeamCreatedBy) VALUES ({0}, {1}, {2}, {3})'.format(teamID, teamName, courseID, teamBy)
+
+        courseID, teamBy = course_get(choosenCourse)
+        
+        cursor.execute('INSERT INTO Teams (TeamName, CourseID, TeamCreatedBy) VALUES ({0}, {1}, {2})'.format(teamName, courseID, teamBy)
         output = cursor.fetchall()
         if output:
             print("team_create was successful.")
@@ -15,12 +18,12 @@ def team_create(teamID, teamName, courseID, teamBy):
 
 #----------------------------------------------------------
  
-def team_update(teamID, teamName, courseID, teamBy):
+def team_update(teamName, courseID, teamBy):
     try:
         print()
         print("Start team_update():")
 
-        cursor.execute('UPDATE Teams SET TeamName = {1}, CourseID = {2}, TeamCreatedBy = {3} WHERE TeamID = {0}'.format(teamID, teamName, courseID, teamBy)      
+        cursor.execute('UPDATE Teams SET CourseID = {1}, TeamCreatedBy = {2} WHERE TeamName = {0}'.format(teamName, courseID, teamBy)      
         output = cursor.fetchall()
         if output:
             print("team_update was successful.")
@@ -33,12 +36,12 @@ def team_update(teamID, teamName, courseID, teamBy):
 
 #----------------------------------------------------------
 
-def team_delete(teamID):
+def team_delete(teamName, courseID, teamBy):
     try:
         print()
         print("Start team_delete():")
 
-        cursor.execute('DELETE FROM Teams WHERE TeamID = {0}'.format(teamID)      
+        cursor.execute('DELETE FROM Teams WHERE TeamName = {0}, CourseID = {1}, TeamCreatedBy = {2}'.format(teamName, courseID, teamBy)      
         output = cursor.fetchall()
         if output:
             print("team_delete was successful.")
